429b983f520327dd66d0a718008884d3c751f6b8
Florian Krause <florian.krause@eventim.de>
2026-03-14 08:08:13 +0100

    add player profile pics and reduce players.js for test purpose

--- src/data/players.js
+++ src/data/players.js
@@ -108,117 +108,117 @@ const players = [
     fantasyPpg: 14.9,
     fantasyFinishOverall: 23,
     fantasyFinishPosition: 1
-  },
-  {
-    id: 11,
-    name: "Lamar Jackson",
-    team: "Baltimore Ravens",
-    teamShort: "BAL",
-    position: "QB",
-    fantasyPoints: 214.9,
-    fantasyPpg: 16.5,
-    fantasyFinishOverall: 40,
-    fantasyFinishPosition: 19
-  },
-  {
-    id: 12,
-    name: "George Pickens",
-    team: "Dallas Cowboys",
-    teamShort: "DAL",
-    position: "WR",
-    fantasyPoints: 245.4,
-    fantasyPpg: 14.4,
-    fantasyFinishOverall: 26,
-    fantasyFinishPosition: 5
-  },
-  {
-    id: 13,
-    name: "George Kittle",
-    team: "San Francisco 49ers",
-    teamShort: "SF",
-    position: "TE",
-    fantasyPoints: 155.2,
-    fantasyPpg: 9.1,
-    fantasyFinishOverall: 92,
-    fantasyFinishPosition: 6
-  },
-  {
-    id: 14,
-    name: "Kenneth Walker III",
-    team: "Seattle Seahawks",
-    teamShort: "SEA",
-    position: "RB",
-    fantasyPoints: 176.4,
-    fantasyPpg: 10.4,
-    fantasyFinishOverall: 65,
-    fantasyFinishPosition: 20
-  },
-  {
-    id: 15,
-    name: "Kyle Pitts",
-    team: "Atlanta Falcons",
-    teamShort: "ATL",
-    position: "TE",
-    fantasyPoints: 166.8,
-    fantasyPpg: 9.8,
-    fantasyFinishOverall: 76,
-    fantasyFinishPosition: 4
-  },
-  {
-    id: 16,
-    name: "Justin Jefferson",
-    team: "Minnesota Vikings",
-    teamShort: "MIN",
-    position: "WR",
-    fantasyPoints: 159.5,
-    fantasyPpg: 9.4,
-    fantasyFinishOverall: 85,
-    fantasyFinishPosition: 19
-  },
-  {
-    id: 17,
-    name: "Derrick Henry",
-    team: "Baltimore Ravens",
-    teamShort: "BAL",
-    position: "RB",
-    fantasyPoints: 272.0,
-    fantasyPpg: 16.0,
-    fantasyFinishOverall: 20,
-    fantasyFinishPosition: 8
-  },
-  {
-    id: 18,
-    name: "Keenan Allen",
-    team: "Chicago Bears",
-    teamShort: "CHI",
-    position: "WR",
-    fantasyPoints: 182.7,
-    fantasyPpg: 10.7,
-    fantasyFinishOverall: 57,
-    fantasyFinishPosition: 12
-  },
-  {
-    id: 19,
-    name: "Romeo Doubs",
-    team: "Green Bay Packers",
-    teamShort: "GB",
-    position: "WR",
-    fantasyPoints: 171.9,
-    fantasyPpg: 10.7,
-    fantasyFinishOverall: 69,
-    fantasyFinishPosition: 14
-  },
-  {
-    id: 20,
-    name: "Patrick Mahomes",
-    team: "Kansas City Chiefs",
-    teamShort: "KC",
-    position: "QB",
-    fantasyPoints: 285.2,
-    fantasyPpg: 20.4,
-    fantasyFinishOverall: 19,
-    fantasyFinishPosition: 8
   }
+  // {
+  //   id: 11,
+  //   name: "Lamar Jackson",
+  //   team: "Baltimore Ravens",
+  //   teamShort: "BAL",
+  //   position: "QB",
+  //   fantasyPoints: 214.9,
+  //   fantasyPpg: 16.5,
+  //   fantasyFinishOverall: 40,
+  //   fantasyFinishPosition: 19
+  // },
+  // {
+  //   id: 12,
+  //   name: "George Pickens",
+  //   team: "Dallas Cowboys",
+  //   teamShort: "DAL",
+  //   position: "WR",
+  //   fantasyPoints: 245.4,
+  //   fantasyPpg: 14.4,
+  //   fantasyFinishOverall: 26,
+  //   fantasyFinishPosition: 5
+  // },
+  // {
+  //   id: 13,
+  //   name: "George Kittle",
+  //   team: "San Francisco 49ers",
+  //   teamShort: "SF",
+  //   position: "TE",
+  //   fantasyPoints: 155.2,
+  //   fantasyPpg: 9.1,
+  //   fantasyFinishOverall: 92,
+  //   fantasyFinishPosition: 6
+  // },
+  // {
+  //   id: 14,
+  //   name: "Kenneth Walker III",
+  //   team: "Seattle Seahawks",
+  //   teamShort: "SEA",
+  //   position: "RB",
+  //   fantasyPoints: 176.4,
+  //   fantasyPpg: 10.4,
+  //   fantasyFinishOverall: 65,
+  //   fantasyFinishPosition: 20
+  // },
+  // {
+  //   id: 15,
+  //   name: "Kyle Pitts",
+  //   team: "Atlanta Falcons",
+  //   teamShort: "ATL",
+  //   position: "TE",
+  //   fantasyPoints: 166.8,
+  //   fantasyPpg: 9.8,
+  //   fantasyFinishOverall: 76,
+  //   fantasyFinishPosition: 4
+  // },
+  // {
+  //   id: 16,
+  //   name: "Justin Jefferson",
+  //   team: "Minnesota Vikings",
+  //   teamShort: "MIN",
+  //   position: "WR",
+  //   fantasyPoints: 159.5,
+  //   fantasyPpg: 9.4,
+  //   fantasyFinishOverall: 85,
+  //   fantasyFinishPosition: 19
+  // },
+  // {
+  //   id: 17,
+  //   name: "Derrick Henry",
+  //   team: "Baltimore Ravens",
+  //   teamShort: "BAL",
+  //   position: "RB",
+  //   fantasyPoints: 272.0,
+  //   fantasyPpg: 16.0,
+  //   fantasyFinishOverall: 20,
+  //   fantasyFinishPosition: 8
+  // },
+  // {
+  //   id: 18,
+  //   name: "Keenan Allen",
+  //   team: "Chicago Bears",
+  //   teamShort: "CHI",
+  //   position: "WR",
+  //   fantasyPoints: 182.7,
+  //   fantasyPpg: 10.7,
+  //   fantasyFinishOverall: 57,
+  //   fantasyFinishPosition: 12
+  // },
+  // {
+  //   id: 19,
+  //   name: "Romeo Doubs",
+  //   team: "Green Bay Packers",
+  //   teamShort: "GB",
+  //   position: "WR",
+  //   fantasyPoints: 171.9,
+  //   fantasyPpg: 10.7,
+  //   fantasyFinishOverall: 69,
+  //   fantasyFinishPosition: 14
+  // },
+  // {
+  //   id: 20,
+  //   name: "Patrick Mahomes",
+  //   team: "Kansas City Chiefs",
+  //   teamShort: "KC",
+  //   position: "QB",
+  //   fantasyPoints: 285.2,
+  //   fantasyPpg: 20.4,
+  //   fantasyFinishOverall: 19,
+  //   fantasyFinishPosition: 8
+  // }
 ];
 
 export default players
